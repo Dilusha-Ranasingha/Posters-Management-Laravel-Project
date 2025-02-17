b227a320754f3d36dfd08e3b7ae4e3c95c270aaf
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-02-18 01:49:35 +0530

    feat: implement user registration with pwd encrypt and store data on database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,6 +13,10 @@ class UserController extends Controller
             'email' => ['required', 'email'],     //required|email is make sure the email is in the correct format
             'password' => ['required', 'min:8', 'max:100'],   //required|min:8|max:15 is make sure the password is at least 8 characters and not more than 15 characters
         ]);
+
+        $incommingFileds['password'] = bcrypt($incommingFileds['password']); //bcrypt() is a helper function that encrypts the password that function is laravel already provided
+        User::create($incommingFileds);      //User::create() is a function that laravel already provided to insert data into the database by using the model 
+
         return 'Thank you for registering!';
     }
 }
