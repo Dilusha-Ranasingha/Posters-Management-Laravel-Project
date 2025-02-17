56722d12d9e7f965b3981171dbd2302476ae0526
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-02-18 02:29:54 +0530

    feat: enhance user registration with unique name,email and auto-login after register and use Blade directives

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,20 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
         $incommingFileds = $request->validate([
-            'name' => ['required', 'string', 'max:25'], //required|string|max:255 is make sure the name is a string and not empty
-            'email' => ['required', 'email'],     //required|email is make sure the email is in the correct format
+            'name' => ['required', 'string', 'max:25', Rule::unique('users','name')], //required|string|max:255 is make sure the name is a string and not empty and unqiue means the name should not be duplicated from the database
+            'email' => ['required', 'email', Rule::unique('users','email')],     //required|email is make sure the email is in the correct format
             'password' => ['required', 'min:8', 'max:100'],   //required|min:8|max:15 is make sure the password is at least 8 characters and not more than 15 characters
         ]);
 
         $incommingFileds['password'] = bcrypt($incommingFileds['password']); //bcrypt() is a helper function that encrypts the password that function is laravel already provided
-        User::create($incommingFileds);      //User::create() is a function that laravel already provided to insert data into the database by using the model 
+        $user = User::create($incommingFileds);      //User::create() is a function that laravel already provided to insert data into the database by using the model 
+        auth()->login($user);   //auth()->login() is a function that laravel already provided to login the user after registration
 
-        return 'Thank you for registering!';
+        return redirect('/');   //redirect() is a function that laravel already provided to redirect the user to the specified page
     }
 }
  
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,11 @@
     <title>HomePage</title>
 </head>
 <body>
+    <!--@auth, @else, and @endauth are called Blade directives in Laravel-->
+    @auth        <!--hecks if a user is logged in. If the user is authenticated-->
+    <h2>congrags you are succsessfully logged!</h2>
+
+    @else        <!--If the user is not authenticated-->
     <div style="text-align: center; border: 1px solid #000; padding: 20px; width: 300px; margin: 0 auto; margin-top: 100px;">
         <h1>Resgister form</h1>
         <form action="/register" method="POST">
@@ -15,5 +20,10 @@
             <button>Register</button>
         </form>
     </div>
+
+
+    @endauth       <!--End of the auth check-->
+    
+
 </body>
 </html>
